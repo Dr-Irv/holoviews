0555b119f6c18c3bcc2d651d8c81348c273f0086
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-02-18 17:03:48 +0000

    Added Labels element

--- holoviews/plotting/bokeh/__init__.py
+++ holoviews/plotting/bokeh/__init__.py
@@ -14,7 +14,7 @@ from ...element import (Curve, Points, Scatter, Image, Raster, Path,
                         ErrorBars, Text, HLine, VLine, Spline, Spikes,
                         Table, ItemTable, Area, HSV, QuadMesh, VectorField,
                         Graph, Nodes, EdgePaths, Distribution, Bivariate,
-                        TriMesh, Violin, Chord, Div)
+                        TriMesh, Violin, Chord, Div, Labels)
 from ...core.options import Options, Cycle, Palette
 from ...core.util import VersionError
 
@@ -29,7 +29,7 @@ except:
     DFrame = None
 
 from .annotation import (TextPlot, LineAnnotationPlot, SplinePlot,
-                         ArrowPlot, DivPlot)
+                         ArrowPlot, DivPlot, LabelsPlot)
 from .callbacks import Callback # noqa (API import)
 from .element import OverlayPlot, ElementPlot
 from .chart import (PointPlot, CurvePlot, SpreadPlot, ErrorPlot, HistogramPlot,
@@ -91,6 +91,7 @@ associations = {Overlay: OverlayPlot,
                 HLine: LineAnnotationPlot,
                 VLine: LineAnnotationPlot,
                 Text: TextPlot,
+                Labels: LabelsPlot,
                 Spline: SplinePlot,
                 Arrow: ArrowPlot,
                 Div: DivPlot,
